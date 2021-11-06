80cc263a228c60b2658fe554ae2efcb91c5466f6
Gabriel Lopez <lopezg3000@gmail.com>
2021-11-06 13:25:35 -0500

    routing added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 //links added but still need to include routes as well as figure out how to make a link active when clicked
 
 
-const links = ['Home', 'Shop', 'Cart', 'About Us', 'Login/Register'];
+const links = [
+    { path: '/home', label: 'Home' },
+    { path: '/shop', label: 'Shop' },
+    { path: '/cart', label: 'Cart' },
+    { path: '/about', label: 'About' },
+    { path: '/login', label: 'Login' }
+];
 
 export default function navbar() {
     return (
         <div>
-            <Nav
-                activeKey="/home"
-            >
+            <Nav>
                 {links.map(link => (
                     <Nav.Item>
-                        <Nav.Link eventKey={link}>{link}</Nav.Link>
+                        <Nav.Link as={NavLink} to={link.path}>{link.label}</Nav.Link>
                     </Nav.Item>
                 ))}
             </Nav>
