fc3af90151ab70e31c39993986c70f638b082448
Gabriel Lopez <lopezg3000@gmail.com>
2022-01-17 10:44:37 -0600

    validation added to registration form onChange

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -11,21 +11,42 @@ export default function Register() {
     const [option, setOption] = useState('');
     const [errors, setErrors] = useState({});
 
+
+    const strongPasswordRegex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+    // const stringPassswordError = new Error("Password must be strong. At least one upper case alphabet. At least one lower case alphabet. At least one digit. At least one special character. Minimum eight in length")
+
     const schema = {
         firstName: Joi.string().min(1).max(20).required(),
         lastName: Joi.string().min(1).max(20).required(),
-        username: Joi.string().email().required(),
-        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+        username: Joi.string().email({ tlds: { allow: false } }).label('Email').required(),
+        password: Joi.string()
+            .pattern(new RegExp(strongPasswordRegex))
+            .message("Password must be strong. At least one upper case alphabet. At least one lower case alphabet. At least one digit. At least one special character. Minimum eight in length")
+            .required(),
         address: Joi.string().required(),
         city: Joi.string().required(),
         state: Joi.string().required(),
         zipCode: Joi.string().required()
-    }
+    };
+
+    const validateProperty = ({ name, value }) => {
+        const obj = { [name]: value };
+        const subSchema = Joi.object({ [name]: schema[name] });
+        const { error } = subSchema.validate(obj);
+
+        return error ? error.details[0].message : null;
+    };
 
     const handleChange = ({ currentTarget: target }) => {
+        const errorsData = { ...errors };
+        const errorMessage = validateProperty(target);
+        if (errorMessage) errorsData[target.name] = errorMessage;
+        else delete errorsData[target.name];
+
         const userClone = { ...user };
         userClone[target.name] = target.value;
         setUser(userClone);
+        setErrors(errorsData);
     };
 
     const handleSelect = (e) => {
@@ -33,7 +54,8 @@ export default function Register() {
         handleChange(e);
     };
 
-    console.log('This is the user state: ', user);
+    // console.log('This is the user state: ', user);
+    // console.log(errors);
     return (
         <Form>
             <Row className="mb-3">
@@ -46,6 +68,11 @@ export default function Register() {
                         placeholder="John"
                     />
                 </Form.Group>
+                {errors.firstName && (
+                    <div className="alert alert-danger">
+                        {errors.firstName}
+                    </div>
+                )}
                 <Form.Group as={Col} controlId="formGridLastName">
                     <Form.Label>Last name</Form.Label>
                     <Form.Control
@@ -55,6 +82,11 @@ export default function Register() {
                         placeholder="Doe"
                     />
                 </Form.Group>
+                {errors.lastName && (
+                    <div className="alert alert-danger">
+                        {errors.lastName}
+                    </div>
+                )}
             </Row>
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridEmail">
@@ -66,6 +98,11 @@ export default function Register() {
                         placeholder="Enter email"
                     />
                 </Form.Group>
+                {errors.username && (
+                    <div className="alert alert-danger">
+                        {errors.username}
+                    </div>
+                )}
             </Row>
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridPassword">
@@ -77,13 +114,11 @@ export default function Register() {
                         placeholder="Password"
                     />
                 </Form.Group>
-
-                {/* //may need to find out how to do this
-                <Form.Group as={Col} controlId="formGridPasswordConfirmation">
-                    <Form.Label>Confirm password</Form.Label>
-                    <Form.Control type="password" />
-                </Form.Group> 
-                */}
+                {errors.password && (
+                    <div className="alert alert-danger">
+                        {errors.password}
+                    </div>
+                )}
             </Row>
 
             <Form.Group className="mb-3" controlId="formGridAddress1">
@@ -95,11 +130,11 @@ export default function Register() {
                     placeholder="1234 Main St"
                 />
             </Form.Group>
-
-            {/* <Form.Group className="mb-3" controlId="formGridAddress2">
-                <Form.Label>Address 2</Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" />
-            </Form.Group> */}
+            {errors.address && (
+                <div className="alert alert-danger">
+                    {errors.address}
+                </div>
+            )}
 
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridCity">
@@ -110,33 +145,47 @@ export default function Register() {
                         type="text"
                     />
                 </Form.Group>
+                {errors.city && (
+                    <div className="alert alert-danger">
+                        {errors.city}
+                    </div>
+                )}
 
                 <Form.Group as={Col} controlId="formGridState">
                     <Form.Label>State</Form.Label>
                     <Form.Select name='state' defaultValue={option} onChange={handleSelect}>
+                        <option value=''>Choose...</option>
                         {states.map(state => (
                             <option key={state.name} value={state.name}>
                                 {state.abbreviation}
                             </option>
                         ))}
-                        <option>Choose...</option>
-                        <option>...</option>
                     </Form.Select>
                 </Form.Group>
+                {errors.state && (
+                    <div className="alert alert-danger">
+                        {errors.state}
+                    </div>
+                )}
 
                 <Form.Group as={Col} controlId="formGridZip">
                     <Form.Label>Zip</Form.Label>
                     <Form.Control
                         name='zipCode'
                         onChange={handleChange}
-                        type="number"
+                        type="text"
                     />
                 </Form.Group>
+                {errors.zipCode && (
+                    <div className="alert alert-danger">
+                        {errors.zipCode}
+                    </div>
+                )}
             </Row>
 
-            <Form.Group className="mb-3" id="formGridCheckbox">
+            {/* <Form.Group className="mb-3" id="formGridCheckbox">
                 <Form.Check type="checkbox" label="Agree to terms and conditions" />
-            </Form.Group>
+            </Form.Group> */}
 
             <Button variant="primary" type="submit">
                 Submit
